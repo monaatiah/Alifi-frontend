300f113348fa0e0fef10294a092729f70463aa14
amir.mashworld@gmail.com <amir.mashworld@gmail.com>
2025-12-10 22:06:05 +0200

    cart

--- pages/cart.js
+++ pages/cart.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { wrapper } from "../../src/store";
+import { wrapper } from "../src/store";
 import { END } from "redux-saga";
 import dynamic from "next/dynamic";
 
@@ -11,44 +11,24 @@ const InnerHead = dynamic(() => import("@/components/inner-head/Index"), {
   ssr: false,
 });
 
-const SingleProduct = dynamic(
-  () => import("@/components/single-product/Index"),
-  {
-    ssr: false,
-  }
-);
-
-const ReviewsSection = dynamic(
-  () => import("@/components/reviews-section/Index"),
-  {
-    ssr: false,
-  }
-);
+const CartSection = dynamic(() => import("@/components/cart/Index"), {
+  ssr: false,
+});
 
 const SingleProductsPage = () => {
   return (
     <>
       <Header />
       <InnerHead />
-      <SingleProduct />
-      <ReviewsSection />
+      <CartSection />
     </>
   );
 };
 
-export async function getStaticPaths() {
-  return {
-    paths: [],
-    fallback: "blocking",
-  };
-}
-
 export const getStaticProps = wrapper.getStaticProps((store) => {
-  return async ({ params }) => {
-    const { id } = params;
+  return async () => {
     store.dispatch(END);
     await store.sagaTask.toPromise();
-
     return {
       props: {},
       revalidate: 1,
---
+++ src/components/cart/Index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import styles from "./styles/styles.module.scss";
+
+const Index = () => {
+  return (
+    <div className={styles["cart-section"]}>
+      <Container></Container>
+    </div>
+  );
+};
+
+export default Index;
